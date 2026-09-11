5ea5394cbc061558af439d4059ea123c82e9162e
BLpranav15 <blpranav2202@gmail.com>
2026-09-11 13:02:41 +0530

    Prepare project for deployment

--- project_grocery/project_grocery/app.py
+++ project_grocery/project_grocery/app.py
@@ -17,7 +17,10 @@ app.secret_key = os.urandom(24).hex()
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 # Configurations
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/grocery'  
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv(
+    'DATABASE_URL',
+    'mysql+pymysql://root:@localhost/grocery'
+) 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize extensions
